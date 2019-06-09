7061a89f51284779a27a0bcbd6adefee5c79652a
franciscoescobar <franciscoescobar@users.noreply.github.com>
2019-06-09 09:34:09 -0300

    deploy docs

---
+++ docs/precache-manifest.1f88f1c0456129e3939fda064012bfc6.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d702dc273c88c3332bf0bb19d1af764b",
+    "url": "/sumate-final/index.html"
+  },
+  {
+    "revision": "5c51414b9609c84b6498",
+    "url": "/sumate-final/static/css/main.81c87b2e.chunk.css"
+  },
+  {
+    "revision": "1d6352d024a6e92672ed",
+    "url": "/sumate-final/static/js/2.0ec6f10e.chunk.js"
+  },
+  {
+    "revision": "5c51414b9609c84b6498",
+    "url": "/sumate-final/static/js/main.da15861e.chunk.js"
+  },
+  {
+    "revision": "ffc30316f538a39d0311",
+    "url": "/sumate-final/static/js/runtime~main.4e12191e.js"
+  },
+  {
+    "revision": "be110202da1b7b632416a9afdaa53ad0",
+    "url": "/sumate-final/static/media/boton.be110202.svg"
+  },
+  {
+    "revision": "b90fd6362e8dad9b690f7ef05b53ccbb",
+    "url": "/sumate-final/static/media/enviar.b90fd636.svg"
+  },
+  {
+    "revision": "f2be5f618b7936cd017e587fafd11315",
+    "url": "/sumate-final/static/media/finalizado.f2be5f61.svg"
+  },
+  {
+    "revision": "8ba8cbed1fb38d078ddb27474560921f",
+    "url": "/sumate-final/static/media/huella.8ba8cbed.svg"
+  },
+  {
+    "revision": "1f43d02bab2ff2be0f73d1dedae1dcb7",
+    "url": "/sumate-final/static/media/lapiz.1f43d02b.svg"
+  },
+  {
+    "revision": "8b32ee1c1673340efbc96f717f577ee0",
+    "url": "/sumate-final/static/media/logo.8b32ee1c.svg"
+  },
+  {
+    "revision": "5132af158b190ded3e4faad0c5d0045b",
+    "url": "/sumate-final/static/media/mano.5132af15.svg"
+  },
+  {
+    "revision": "f7b31a19211421792efe1fc6006ba3d7",
+    "url": "/sumate-final/static/media/ocean.f7b31a19.mp4"
+  },
+  {
+    "revision": "921dab27ca6e370eb1ae93b88e006d49",
+    "url": "/sumate-final/static/media/sumate.921dab27.svg"
+  }
+]);
